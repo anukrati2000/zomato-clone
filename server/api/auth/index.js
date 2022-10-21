aa498f72ca88834ee52577d9322cb4d4c818c4bc
anukrati2000 <rathoreanukrati09@gmail.com>
2022-10-21 16:41:36 +0530

    completed server

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from 'passport';
 
 import { UserModel } from '../../database/allModels';
 import { ValidateSignup, ValidateSignin } from '../../validation/auth.validation';
@@ -45,5 +46,45 @@ Router.post('/signin', async (req, res) => {
         return res.status(500).json({ error: error.message });
     }
 });
+
+Router.get(
+    "'/google'",
+    passport.authenticate('google', {
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email',
+        ],
+    })
+);
+
+Router.get(
+    '/google/callback',
+    // passport.authenticate('google', { failureRedirect: "/" }),
+    // (req, res) => {
+    //     return res.status(200).json({
+    //         token: req.session.passport.user.token  
+    //     });
+    // }
+
+    passport.authenticate(
+        'google',
+        {
+            scope:
+                [
+                    'https://www.googleapis.com/auth/userinfo.profile',
+                    'https://www.googleapis.com/auth/userinfo.email',
+                ],
+        },
+    ),
+    (req, res) => {
+        // return res.status(200).json({
+        //     token: req.session.passport.user.token
+        // });
+
+        return res.redirect(
+            `http://localhost:3000/google/${req.session.passport.user.token}`
+        );
+    }
+);
 
 export default Router;
