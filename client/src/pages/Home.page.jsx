81f163a02c6f86aa9d33d1fd694bee328e384e69
anukrati2000 <rathoreanukrati09@gmail.com>
2022-11-01 21:32:43 +0530

    Setted up Routing system

---
+++ client/src/pages/Home.page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Layout
+import HomeLayout from '../layouts/Homepage.layout';
+
+const Home = () => {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default HomeLayout(Home);
